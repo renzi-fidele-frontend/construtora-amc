c851e76be80809bc52776cfe43d6b3f58b37e489
Renzi Fidele <renzifidele2001@gmail.com>
2026-01-04 13:39:28 +0200

    feat: Adicionando a parte inferior do cabeçalho do site

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,12 +1,32 @@
 import Image from "next/image";
 import Container from "./Container";
-import { UserStar } from "lucide-react";
+import { CircleUserRound, ShieldUser, UserStar } from "lucide-react";
 import Nav from "./Nav";
 import Link from "next/link";
 
+const categorias = [
+   {
+      nome: "Empreendimentos",
+      url: "#",
+   },
+   {
+      nome: "Pré-lançamentos",
+      url: "#",
+   },
+   {
+      nome: "Lançamentos",
+      url: "#",
+   },
+   {
+      nome: "Pronto para morar",
+      url: "#",
+   },
+];
+
 const Header = () => {
    return (
-      <header className="flex items-center justify-between">
+      <header>
+         {/* Superior */}
          <Container className="w-full flex items-center justify-between">
             <Image className="my-2" width={194} height={73} src="/img/logo.png" alt="Ilustração do logotipo da marca Amc Contruções" />
             <div className="flex gap-7 items-stretch">
@@ -15,7 +35,7 @@ const Header = () => {
                {/* CTA da Área do cliente */}
                <Link href="#">
                   <div className="text-white bg-theme2 px-[26] py-[22] flex gap-3.5 items-center font-medium">
-                     <UserStar className="size-12" />
+                     <UserStar className="size-11" />
                      <div className="h-[55] border-s border-white"></div>
                      <p className="text-[16px] leading-6">
                         Área do <br />
@@ -25,6 +45,34 @@ const Header = () => {
                </Link>
             </div>
          </Container>
+         {/* Inferior */}
+         <div className="py-2.5 bg-theme1 text-white text-lg font-medium uppercase">
+            <Container className="flex items-center justify-center gap-20">
+               {categorias.map((v, k) => (
+                  <Link href={v.url} key={k}>
+                     {v.nome}
+                  </Link>
+               ))}
+               {/* CTA para o minha casa minha vida */}
+               <div className="flex gap-10 items-center">
+                  <div className="border-s border-dashed h-6"></div>
+                  <div className="flex items-center gap-2 text-[11px] font-bold">
+                     <Image
+                        width={105}
+                        height={74}
+                        className="w-auto object-contain h-8.75"
+                        src="/img/minha-casa.webp"
+                        alt="Ilustração da logo marca do minha casa minha vida"
+                     />
+                     <div className="leading-2">
+                        <p>Minha Casa</p>
+                        <br />
+                        <p>Minha Vida</p>
+                     </div>
+                  </div>
+               </div>
+            </Container>
+         </div>
       </header>
    );
 };
