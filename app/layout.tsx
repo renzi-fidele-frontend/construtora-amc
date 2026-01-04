c089393a47095987dc3c5b2d7217f4a52a7593e8
Renzi Fidele <renzifidele2001@gmail.com>
2026-01-04 10:42:04 +0200

    feat: Atualizando a fonte do site para Rubik e adicionando mais palavras-chave ao metadados de SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Rubik } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/layout/Header";
 
-const geistSans = Geist({
-   variable: "--font-geist-sans",
-   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-   variable: "--font-geist-mono",
+const fontRubik = Rubik({
+   variable: "--font-rubik",
    subsets: ["latin"],
 });
 
@@ -17,7 +13,7 @@ export const metadata: Metadata = {
    description:
       "Sediada em Londrina-PR e com filial em Palhoça-SC, a AMC Construções reúne a tradição de um grupo com 29 anos de história. Estruturada por áreas de engenharia e atuando em sintonia com uma linha estratégica de mercado, a AMC Construções é uma empresa moderna e diferenciada.",
    authors: [{ name: "Renzi Fidele", url: "https://github.com/renzi-fidele-frontend/" }],
-   keywords: ["amc", "amc construções"],
+   keywords: ["amc", "amc construções", "construtora amc", "venda de imóveis"],
 };
 
 export default function RootLayout({
@@ -27,7 +23,10 @@ export default function RootLayout({
 }>) {
    return (
       <html lang="pt-BR">
-         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+         <body className={`${fontRubik.variable} antialiased min-h-dvh`}>
+            <Header />
+            {children}
+         </body>
       </html>
    );
 }
