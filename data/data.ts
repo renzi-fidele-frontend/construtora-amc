8362a501ed4dea117bd692502dd0b398eef104e5
Renzi Fidele <renzifidele2001@gmail.com>
2026-01-27 20:18:48 +0200

    feat: Adicionando a estrutura básica formulário de contato da página de parceria

--- data/data.ts
+++ data/data.ts
@@ -368,36 +368,6 @@ export const clientes: ICliente[] = [
    },
 ];
 
-export const estadosBrasil = [
-   "Acre",
-   "Alagoas",
-   "Amapá",
-   "Amazonas",
-   "Bahia",
-   "Ceará",
-   "Distrito Federal",
-   "Espírito Santo",
-   "Goiás",
-   "Maranhão",
-   "Mato Grosso",
-   "Mato Grosso do Sul",
-   "Minas Gerais",
-   "Pará",
-   "Paraíba",
-   "Paraná",
-   "Pernambuco",
-   "Piauí",
-   "Rio de Janeiro",
-   "Rio Grande do Norte",
-   "Rio Grande do Sul",
-   "Rondônia",
-   "Roraima",
-   "Santa Catarina",
-   "São Paulo",
-   "Sergipe",
-   "Tocantins",
-];
-
 export const cidadesPorEstado: { nome: string; cidades: string[] }[] = [
    { nome: "Acre", cidades: ["Rio Branco", "Cruzeiro do Sul", "Senador Guiomard"] },
    { nome: "Alagoas", cidades: ["Maceió", "Arapiraca", "Palmeira dos Índios"] },
