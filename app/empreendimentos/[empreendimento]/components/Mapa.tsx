c6ca7fd67e39d05d16480718f094653eb3dadef0
Renzi Fidele <renzifidele2001@gmail.com>
2026-01-30 22:19:15 +0200

    feat: Adicionando o componente Mapa com apresentado a localização do empreendimento

---
+++ app/empreendimentos/[empreendimento]/components/Mapa.tsx
@@ -0,0 +1,69 @@
+"use client";
+import useRoutesService from "@/hooks/useRoutesService";
+import { Map, useMap } from "@vis.gl/react-google-maps";
+import { useState } from "react";
+
+interface ILugar {
+   lat: number;
+   lng: number;
+}
+const Mapa = ({ localizacaoDoEmpreendimento }: { localizacaoDoEmpreendimento: ILugar }) => {
+   const [localizacaoDoUsuario, setLocalizacao] = useState<ILugar>();
+   const { directionsService, directionsRenderer } = useRoutesService();
+   // TODO: Mais tarde renderizar os detalhes do caminho até o empreendimento
+   const [caminho, setCaminho] = useState<google.maps.DirectionsResult>();
+   const map = useMap();
+
+   // TODO: Encontrar a localizacao do usuario apos se iniciar a busca pelo destino do empreendimento
+   function encontrarLocalizacaoDoUsuario() {
+      // Apanhando as cooordenadas do usuário
+      navigator.geolocation.getCurrentPosition(
+         async (position) => {
+            const lat = position.coords.latitude;
+            const lng = position.coords.longitude;
+            setLocalizacao({ lat, lng });
+         },
+         (error) => {
+            console.log("Erro ao obter a localização do usuário");
+         },
+      );
+   }
+
+   // TODO: Adicionar a funcionalidade de encontrar o caminho até o empreendimento
+   async function encontrarDirecao(destino: ILugar) {
+      if (localizacaoDoUsuario) {
+         await directionsService.route(
+            {
+               origin: localizacaoDoUsuario,
+               destination: destino,
+               travelMode: google.maps.TravelMode.DRIVING,
+            },
+            (result, status) => {
+               directionsRenderer.setMap(map);
+               directionsRenderer.setDirections(result);
+               directionsRenderer.setOptions({
+                  markerOptions: {
+                     visible: false,
+                  },
+                  polylineOptions: {
+                     strokeColor: "red",
+                  },
+               });
+               if (result) setCaminho(result);
+            },
+         );
+      }
+   }
+
+   // TODO: Adicionar o marker contendo as informações básica sobre o empreendimento
+   return (
+      <Map
+         mapId="d95c984c2c99e484fcaaf9b5"
+         className="w-[70%] h-140!"
+         defaultZoom={15}
+         defaultCenter={localizacaoDoEmpreendimento}
+         mapTypeId="hybrid"
+      ></Map>
+   );
+};
+export default Mapa;
