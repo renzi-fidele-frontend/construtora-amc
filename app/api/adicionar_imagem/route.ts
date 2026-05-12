36194e36bec89605c6ab4119b2eb74e51576a8cd
Renzi Fidele <renzifidele2001@gmail.com>
2026-05-12 12:03:29 +0200

    feat: Enviando a imagem recebida no backend para o cloundinary e retornando os dados

--- app/api/adicionar_imagem/route.ts
+++ app/api/adicionar_imagem/route.ts
@@ -1,9 +1,30 @@
 import { NextRequest, NextResponse } from "next/server";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+   api_key: process.env.CLOUDINARY_API_KEY,
+   api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 export async function POST(req: NextRequest) {
    const data = await req.formData();
-   const file = data.get("foto");
-   console.log(file);
+   const file = data.get("foto") as File;
 
-   return NextResponse.json({ message: "Imagem recebida com sucesso no backend!" });
+   if (!file) return NextResponse.json({ error: "Ficheiro não encontrado" }, { status: 400 });
+
+   const buffer = Buffer.from(await file.arrayBuffer());
+
+   const carregarImagem = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+         .upload_stream({ folder: "AMC Contruções" }, (error, result) => {
+            if (error) reject(error);
+            else resolve(result);
+         })
+         .end(buffer);
+   });
+
+   console.log(carregarImagem);
+
+   return NextResponse.json({ message: "Imagem recebida com sucesso no backend!", foto: carregarImagem });
 }
