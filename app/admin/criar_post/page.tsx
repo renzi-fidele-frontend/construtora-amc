b0501c0c5360da5617025bb154a105348f39cbe8
Renzi Fidele <renzifidele2001@gmail.com>
2026-05-14 11:42:56 +0200

    feat: Adicionando o campo de imagem do thumbnail ao formulário de criação de artigos do blog

--- app/admin/criar_post/page.tsx
+++ app/admin/criar_post/page.tsx
@@ -6,10 +6,31 @@ import { Input } from "@/components/ui/input";
 
 // TODO: Adicionar a funcionalidade de publicar um artigo
 export default function CriarPost() {
+   const [previaThumbanil, setPreviaThumbnail] = useState<ArrayBuffer | string | null>(null);
+   const [previaDestaque, setPreviaDestaque] = useState<ArrayBuffer | string | null>(null);
+
+   function renderizarPreviaThumbnail(e: ChangeEvent<HTMLInputElement>) {
+      if (!e.target.files) return;
+      const reader = new FileReader();
+      reader.onload = () => {
+         setPreviaThumbnail(reader.result);
+      };
+      reader.readAsDataURL(e.target.files[0]);
+   }
+
+   function renderizarPreviaDestaque(e: ChangeEvent<HTMLInputElement>) {
+      if (!e.target.files) return;
+      const reader = new FileReader();
+      reader.onload = () => {
+         setPreviaDestaque(reader.result);
+      };
+      reader.readAsDataURL(e.target.files[0]);
+   }
+
    return (
       <Container className="py-25">
          <SectionIntro titulo="Adicione um artigo" descricao="Adicione um artigo para o blog da construtora AMC" className="items-start!" />
-         <form className="[&_input]:border-theme1 [&_fieldset]:flex [&_fieldset]:flex-col [&_fieldset]:gap-1.5 **:data-placeholder:text-theme1!">
+         <form className="flex flex-col gap-5 [&_input]:border-theme1 [&_fieldset]:flex [&_fieldset]:flex-col [&_fieldset]:gap-1.5 **:data-placeholder:text-theme1!">
             {/* Título */}
             <fieldset>
                <label htmlFor="titulo">Titulo do artigo</label>
@@ -22,6 +43,31 @@ export default function CriarPost() {
             </fieldset>
 
             {/* Thumbnail */}
+            <fieldset>
+               <label>
+                  Foto do thumbnail <small>(370x290)</small>
+               </label>
+               <label htmlFor="thumbnail" className="w-fit cursor-pointer hover:opacity-80 hover:text-theme2 transition group relative">
+                  <div className="p-15 bg-zinc-300 w-fit">
+                     <div className="rounded border-3 border-theme1 group-hover:border-theme2 transition border-dashed p-8 w-fit mx-auto">
+                        <Plus className="size-14" />
+                     </div>
+                     <p className="mt-4">Clique para carregar uma imagem</p>
+                  </div>
+                  {/* Prévia */}
+                  {previaThumbanil && (
+                     <Image
+                        className="absolute inset-0 w-full h-full object-cover"
+                        src={String(previaThumbanil)}
+                        alt="Thumbnail do artigo"
+                        width={300}
+                        height={300}
+                     />
+                  )}
+               </label>
+               {/* Input invisível */}
+               <input className="hidden" type="file" name="thumbnail" accept="image/*" id="thumbnail" onChange={renderizarPreviaThumbnail} />
+            </fieldset>
             {/* Foto de destaque */}
 
             <fieldset>
