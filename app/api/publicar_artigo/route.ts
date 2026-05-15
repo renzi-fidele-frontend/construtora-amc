09a40a575e2debf41dbff358a929390fe2cb7d85
Renzi Fidele <renzifidele2001@gmail.com>
2026-05-15 09:57:55 +0200

    feat: Enviando o thumbnail e a foto de destaque do artigo ao cloudinary antes de salvar no mongoDB

--- app/api/publicar_artigo/route.ts
+++ app/api/publicar_artigo/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import { v2 as cloudinary } from "cloudinary";
 
 export async function POST(req: NextRequest) {
    const data = await req.formData();
@@ -10,6 +11,31 @@ export async function POST(req: NextRequest) {
 
    try {
       // Enviar o thumbnail para o cloudinary
+      const buffer_thumbnail = Buffer.from(await thumbnail.arrayBuffer());
+      const carregarThumbnail = await new Promise((resolve, reject) => {
+         cloudinary.uploader
+            .upload_stream({ folder: "AMC Contruções" }, (error, result) => {
+               if (error) reject(error);
+               else resolve(result);
+            })
+            .end(buffer_thumbnail);
+      });
+
+      // Enviar a foto de destaque para o cloudinary
+      const buffer_destaque = Buffer.from(await destaque.arrayBuffer());
+      const carregarDestaque = await new Promise((resolve, reject) => {
+         cloudinary.uploader
+            .upload_stream({ folder: "AMC Contruções" }, (error, result) => {
+               if (error) reject(error);
+               else resolve(result);
+            })
+            .end(buffer_destaque);
+      });
+
+      
+      
+      
+
       // Enviar o destaque para o cloudinary
       // Salvar o artigo no banco de dados
       // Retornar a resposta
