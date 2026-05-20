3ef3ce69a0872342664b8dc132ce6ec165c483ab
Renzi Fidele <renzifidele2001@gmail.com>
2026-05-20 19:38:08 +0200

    feat: Adicionando a utilidade de gerar arrays com base no tamanho

--- lib/utils.ts
+++ lib/utils.ts
@@ -28,3 +28,8 @@ export function slugify(text: string) {
            .replace(/--+/g, "-")
       : "";
 }
+
+/** Gerar um array contendo Integers de 1 até o length. */
+export function gerarArray(length = 0) {
+   return Array.from({ length }, (v, i) => i + 1);
+}
