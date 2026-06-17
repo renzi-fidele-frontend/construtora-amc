1fc98db6ef79dc061fb1663fc17627823642fe77
Renzi Fidele <renzifidele2001@gmail.com>
2026-06-17 20:47:32 +0200

    feat: Adicionando o schema json da construtora e do site para super melhoria do SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,10 +63,63 @@ export default async function RootLayout({
    children: React.ReactNode;
 }>) {
    const user = await getLogedUser();
+   const schemaDaConstrutora = {
+      "@context": "http://schema.org",
+      "@type": "Organization",
+      name: "AMC Construções",
+      url: "https://amc.eng.br",
+      logo: "https://amc.eng.br/img/logo.png",
+      contactPoint: {
+         "@type": "ContactPoint",
+         telephone: "+55-47-99152-0164",
+         contactType: "sales",
+         areaServed: "BR",
+         availableLanguage: "Portuguese",
+      },
+      address: [
+         {
+            "@type": "PostalAddress",
+            streetAddress: "Av. Tiradentes, 501 Torre 1 (14º Andar - Sala 1401)",
+            addressLocality: "Londrina",
+            addressRegion: "PR",
+            postalCode: "86070-545",
+            addressCountry: "BR",
+         },
+         {
+            "@type": "PostalAddress",
+            streetAddress: "R. Monza, 226 - Sala 704",
+            addressLocality: "Palhoça",
+            addressRegion: "SC",
+            postalCode: "88132-147",
+            addressCountry: "BR",
+         },
+      ],
+      sameAs: [
+         "https://www.facebook.com/people/AMC-Constru%C3%A7%C3%B5es/100063671165105/",
+         "https://www.instagram.com/construcoesamc/",
+         "https://www.youtube.com/@AMCConstrucoes",
+      ],
+   };
+   const schemaDoSite = {
+      "@context": "https://schema.org",
+      "@type": "WebSite",
+      name: "AMC Construções",
+      url: "https://amc.eng.br",
+      potentialAction: {
+         "@type": "SearchAction",
+         target: {
+            "@type": "EntryPoint",
+            urlTemplate: "https://amc.eng.br/empreendimentos?categoria={search_term_string}",
+         },
+         "query-input": "required name=search_term_string",
+      },
+   };
 
    return (
       <html lang="pt-BR">
          <body className={`${fontRubik.variable} font-rubik antialiased text-theme1 min-h-dvh select-none`}>
+            <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaDaConstrutora) }} />
+            <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaDoSite) }} />
             <Header />
             <TooltipProvider>{children}</TooltipProvider>
             <Footer />
