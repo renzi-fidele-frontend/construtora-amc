e8be9f5a5451add2d8fb5e07097e4bb02931100d
Renzi Fidele <renzifidele2001@gmail.com>
2026-08-03 19:10:07 +0200

    feat: Adicionando a ilustração demonstrando que nenhum artigo foi encontrado na pesquisa

--- app/blog/search/page.tsx
+++ app/blog/search/page.tsx
@@ -17,31 +17,40 @@ export default async function BlogSearchPage({ searchParams }: { searchParams: P
             Resultados da busca por: <span className="underline">{query}</span>
          </h1>
          {/* Listagem */}
-         <div className={`grid sm:grid-cols-2 gap-5`}>
-            {artigos.map((artigo, k) => (
-               <Link
-                  href={`/blog/${artigo.slug}`}
-                  className="border transition hover:bg-theme1 hover:text-white hover:outline-2 outline-theme1"
-                  key={k}
-               >
-                  <Image
-                     src={artigo.thumbnail.secure_url}
-                     width={artigo.thumbnail.width}
-                     height={artigo.thumbnail.height}
-                     alt="Ilustração do artigo"
-                     className="h-60 object-left object-cover"
-                  />
-                  <div className="py-5 xl:py-7 px-5 text-sm sm:text-base">
-                     {/* Data de publicação */}
-                     <p className="uppercase">{dayjs(artigo.publicadoEm).format("DD/MMMM/YYYY")}</p>
-                     {/* Titulo */}
-                     <h3 className="font-bold text-lg md:text-xl line-clamp-2">{artigo.titulo}</h3>
-                     {/* Descrição */}
-                     <p className="line-clamp-4 mt-2">{artigo.descricao}</p>
-                  </div>
-               </Link>
-            ))}
-         </div>
+         {artigos.length > 0 ? (
+            <div className={`grid sm:grid-cols-2 gap-5`}>
+               {artigos.map((artigo, k) => (
+                  <Link
+                     href={`/blog/${artigo.slug}`}
+                     className="border transition hover:bg-theme1 hover:text-white hover:outline-2 outline-theme1"
+                     key={k}
+                  >
+                     <Image
+                        src={artigo.thumbnail.secure_url}
+                        width={artigo.thumbnail.width}
+                        height={artigo.thumbnail.height}
+                        alt="Ilustração do artigo"
+                        className="h-60 object-left object-cover"
+                     />
+                     <div className="py-5 xl:py-7 px-5 text-sm sm:text-base">
+                        {/* Data de publicação */}
+                        <p className="uppercase">{dayjs(artigo.publicadoEm).format("DD/MMMM/YYYY")}</p>
+                        {/* Titulo */}
+                        <h3 className="font-bold text-lg md:text-xl line-clamp-2">{artigo.titulo}</h3>
+                        {/* Descrição */}
+                        <p className="line-clamp-4 mt-2">{artigo.descricao}</p>
+                     </div>
+                  </Link>
+               ))}
+            </div>
+         ) : (
+            <>
+               <div className="pt-4 flex flex-col items-center text-center gap-6">
+                  <Image className="w-auto h-auto" width={500} height={500} src="/img/ilustracao/noSearch.png" alt="Nenhum artigo encontrado" />
+                  <p className="text-lg bg-theme1 text-white px-6 py-2">Nenhum artigo encontrado!</p>
+               </div>
+            </>
+         )}
       </div>
    );
 }
