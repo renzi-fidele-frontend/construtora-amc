f93881a470fd8d284a07a22b84da746c3392b7f2
Renzi Fidele <renzifidele2001@gmail.com>
2026-08-03 20:06:09 +0200

    feat: Adicionando os metadados da página dos resultados da busca por artigos no blog

--- app/blog/search/page.tsx
+++ app/blog/search/page.tsx
@@ -1,10 +1,39 @@
 import BlogSearchBar from "@/components/shared/BlogSearchBar";
 import Artigo from "@/models/Artigo";
 import dayjs from "dayjs";
+import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 
 dayjs.locale("pt-br");
+
+export async function generateMetadata({
+   searchParams,
+}: {
+   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}): Promise<Metadata> {
+   const query = (await searchParams).text;
+
+   return {
+      title: `Resultados da busca por "${query}"`,
+
+      description: `Confira os artigos encontrados para a pesquisa "${query}" no blog da AMC Construções.`,
+      alternates: {
+         canonical: `https://amc.eng.br/blog/search?text=${encodeURIComponent(String(query))}`,
+      },
+
+      openGraph: {
+         title: `Resultados para "${query}" | Blog AMC Construções`,
+         description: `Veja os artigos relacionados à pesquisa ${query}.`,
+         url: `https://amc.eng.br/blog/pesquisa?text=${encodeURIComponent(String(query))}`,
+         type: "website",
+      },
+      robots: {
+         index: false,
+         follow: true,
+      },
+   };
+}
 
 export default async function BlogSearchPage({ searchParams }: { searchParams: Promise<{ [key: string]: string | string[] | undefined }> }) {
    const query = (await searchParams).text;
@@ -46,7 +75,7 @@ export default async function BlogSearchPage({ searchParams }: { searchParams: P
          ) : (
             <>
                <div className="pt-4 flex flex-col items-center text-center gap-6">
-                  <Image className="w-auto h-auto" width={500} height={500} src="/img/ilustracao/noSearch.png" alt="Nenhum artigo encontrado" />
+                  <Image width={474} height={414} src="/img/ilustracao/noSearch.png" alt="Nenhum artigo encontrado" />
                   <p className="text-lg bg-theme1 text-white px-6 py-2">Nenhum artigo encontrado!</p>
                </div>
             </>
