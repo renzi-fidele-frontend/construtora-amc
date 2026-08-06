35097a94e20935a19c039a63d01d332df8bc0b23
Renzi Fidele <renzifidele2001@gmail.com>
2026-08-06 18:50:31 +0200

    feat: Adicionando o nome da categoria ao card do artigo do blog

--- components/shared/CardArtigoBlog.tsx
+++ components/shared/CardArtigoBlog.tsx
@@ -7,7 +7,10 @@ dayjs.locale("pt-br");
 
 const CardArtigoBlog = ({ artigo }: { artigo: IArtigo }) => {
    return (
-      <Link href={`/blog/${artigo.slug}`} className="border transition hover:bg-theme1 hover:text-white hover:outline-2 outline-theme1 relative group">
+      <Link
+         href={`/blog/${artigo.slug}`}
+         className="border transition hover:bg-theme1 hover:text-white hover:outline-2 outline-theme1 relative group"
+      >
          <Image
             src={artigo.thumbnail.secure_url}
             width={artigo.thumbnail.width}
@@ -18,6 +21,10 @@ const CardArtigoBlog = ({ artigo }: { artigo: IArtigo }) => {
          <div className="py-5 xl:py-7 px-5 text-sm sm:text-base">
             {/* Data de publicação */}
             <p className="uppercase">{dayjs(artigo.publicadoEm).format("DD/MMMM/YYYY")}</p>
+            {/* Categoria */}
+            <p className="w-fit px-2 rounded text-white mb-2 text-sm" style={{ background: artigo.categoria.cor }}>
+               {artigo.categoria.nome}
+            </p>
             {/* Titulo */}
             <h3 className="font-bold text-lg md:text-xl line-clamp-2">{artigo.titulo}</h3>
             {/* Descrição */}
@@ -25,7 +32,13 @@ const CardArtigoBlog = ({ artigo }: { artigo: IArtigo }) => {
          </div>
          {/* Categoria */}
          <div className="absolute p-2 sm:p-3 top-0 right-0" style={{ background: artigo.categoria.cor }}>
-            <Image className="size-9 sm:size-11 invert-100" width={300} height={300} src={`/icons/categorias-blog/${artigo.categoria.slug}.png`} alt="Ilustracao da categoria" />
+            <Image
+               className="size-9 sm:size-11 invert-100"
+               width={300}
+               height={300}
+               src={`/icons/categorias-blog/${artigo.categoria.slug}.png`}
+               alt="Ilustracao da categoria"
+            />
          </div>
       </Link>
    );
