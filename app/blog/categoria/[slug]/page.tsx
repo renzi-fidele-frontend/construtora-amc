1d051b2628ce3fb197d43974c0e1f0eeef11cf55
Renzi Fidele <renzifidele2001@gmail.com>
2026-08-06 20:06:39 +0200

    feat: Melhorando a responsividade da seção das categorias do blog

--- app/blog/categoria/[slug]/page.tsx
+++ app/blog/categoria/[slug]/page.tsx
@@ -10,18 +10,17 @@ export default async function Page({ params, searchParams }: Props) {
    const search = await searchParams;
    const page = Number(search.page) || 1;
    const { artigos, categoria, totalPaginas } = await apanhar_artigos_de_categoria(slug, page, 6);
-   console.log(categoria);
 
    return (
       <div>
          <BlogSearchBar />
-         <h1 className="font-medium text-2xl sm:text-3xl md:text-4xl mb-3 sm:mb-3">
+         <h1 className="font-medium text-xl sm:text-2xl md:text-3xl lg:text-4xl mb-2 sm:mb-3">
             Artigos da categoria:{" "}
             <span style={{ color: categoria.cor }} className="underline">
                {categoria.nome}
             </span>
          </h1>
-         <p className="text-lg mb-6">{categoria.descricao}</p>
+         <p className="sm:text-lg mb-8">{categoria.descricao}</p>
          {/* Listagem dos artigos */}
          <div className="grid sm:grid-cols-2 gap-5 mb-7">
             {artigos.map((artigo, k) => (
